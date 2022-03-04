0e84955c2ea79652243d8db2775be84b959ec793
REDDYDILIP22 <100350388+REDDYDILIP22@users.noreply.github.com>
2022-03-04 12:18:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome React application Deployments for multiple ENVS
+          Welcome React application Deployments for multiple environment
         </p>
         <a
           className="App-link"
